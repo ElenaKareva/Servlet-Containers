9b16426610dcc5ca14ca92fa9edc05c25b777756
ElenaKareva <92863528+ElenaKareva@users.noreply.github.com>
2022-06-14 19:48:15 +0300

    bugs fixed

--- src/main/java/ru/netology/repository/PostRepository.java
+++ src/main/java/ru/netology/repository/PostRepository.java
@@ -13,40 +13,46 @@ import java.util.concurrent.atomic.AtomicLong;
 
 
 public class PostRepository {
-    private final ConcurrentHashMap<Long, Post> posts;
-    private final AtomicLong idCounter = new AtomicLong(0L);
+  private final ConcurrentHashMap<Long, Post> posts;
+  private final AtomicLong idCounter = new AtomicLong(0L);
 
-    public PostRepository() {
-        posts = new ConcurrentHashMap<>();
+  public PostRepository() {
+    posts = new ConcurrentHashMap<>();
+  }
+
+  public List<Post> all() {
+    return new ArrayList<>(posts.values());
+  }
+
+  public Optional<Post> getById(long id) {
+    return Optional.ofNullable(posts.get(id));
+
+  }
+
+  public Post save(Post post) {
+
+    if(post.getId() != 0) {
+      if (!posts.containsKey(post.getId())) {
+        throw new NotFoundException();
+      } else {
+        posts.put(post.getId(), post);
+      }
     }
 
-    public List<Post> all() {
-        return new ArrayList<>(posts.values());
+    if (post.getId() == 0) {
+      var newId = idCounter.incrementAndGet();
+      post.setId(newId);
+      posts.put(post.getId(), post);
     }
+    return post;
+  }
 
-    public Optional<Post> getById(long id) {
-        return Optional.ofNullable(posts.get(id));
 
-    }
-
-    public Post save(Post post) {
-        if (post.getId() == 0) {
-            long id = idCounter.incrementAndGet();
-            ;
-            post.setId(id);
-            posts.put(id, post);
-        } else if (post.getId() != 0) {
-            Long currentId = post.getId();
-            posts.put(currentId, post);
-        }
-        return post;
-    }
-
-    public void removeById(long id) {
+      public void removeById ( long id){
         if (posts.containsKey(id)) {
-            posts.remove(id);
+          posts.remove(id);
         } else {
-            throw new NotFoundException("Указанный id не найден");
+          throw new NotFoundException("Указанный id не найден");
         }
+      }
     }
-}
